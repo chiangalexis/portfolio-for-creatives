1365c12dc65447b973251bff252531b14fde4d74
chiangalexis <chiangjietong@gmail.com>
2025-11-08 16:30:47 -0800

    Filters functional

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,21 @@
-$(document).ready(function () {
-  $(".experiment-image-1").click(function () {
-    $(".experiment-image-2").fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
+const filterButtons = document.querySelectorAll(".filter-btn");
+const galleryItems = document.querySelectorAll(".work-page-grid-item");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    filterButtons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    const filterValue = button.dataset.filter;
+    galleryItems.forEach((item) => {
+      if (filterValue === "all" || item.classList.contains(filterValue)) {
+        item.classList.remove("hidden");
+      } else {
+        item.classList.add("hidden");
+      }
+    });
   });
 });
